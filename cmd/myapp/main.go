0922cc2417735d31ee7b1677ab6b6ddf7ab68ea1
Vyacheslav Galinov <galin0ff@yandex.ru>
2023-12-10 20:40:43 +0300

    Instant init customer

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -6,7 +6,12 @@ import (
 )
 
 func main() {
-	var cust internal.Customer
+	cust := internal.Customer{
+		Age:     23,
+		Balance: 10000,
+		Debt:    1000,
+		Name:    "Dmitry",
+	}
 
 	fmt.Printf("%+v\n", cust)
 }

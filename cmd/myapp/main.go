2fef7ba903e9c902d7fb03cc66fb22ee9733a137
Vyacheslav Galinov <galin0ff@yandex.ru>
2023-12-10 20:43:27 +0300

    Anon func for customer disc

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"v2/internal"
 )
 
 func main() {
+
+	const DEFAULT_DISCOUNT = 500
+
 	cust := internal.Customer{
 		Age:     23,
 		Balance: 10000,
@@ -13,5 +17,18 @@ func main() {
 		Name:    "Dmitry",
 	}
 
-	fmt.Printf("%+v\n", cust)
+	cust.CalcDiscount = func() (int, error) {
+		if !cust.Discount {
+			return 0, errors.New("discount not available")
+		}
+		result := DEFAULT_DISCOUNT - cust.Debt
+		if result < 0 {
+			return 0, nil
+		}
+		return result, nil
+	}
+
+	discount, _ := cust.CalcDiscount()
+
+	fmt.Printf("%d", discount)
 }

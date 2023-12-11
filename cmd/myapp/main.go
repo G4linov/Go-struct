3b7c31d4e19d802e891036c56983987905e25491
Vyacheslav Galinov <galin0ff@yandex.ru>
2023-12-11 17:36:32 +0300

    some methods for struct, hide some val from main pac

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -1,41 +1,15 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"v2/internal"
 )
-
-func CalcPrice(cust internal.Customer, price int) (int, error) {
-	if cust.Discount {
-		return (price - 100), nil
-	}
-	return 0, errors.New("discount not available")
-}
 
 func main() {
 
 	cust := internal.NewCustomer("dmitry", 23, 10000, 1000, true)
 
-	const DEFAULT_DISCOUNT = 500
+	_ = cust.WrOffDebt()
 
-	cust.CalcDiscount = func() (int, error) {
-		if !cust.Discount {
-			return 0, errors.New("discount not available")
-		}
-		result := DEFAULT_DISCOUNT - cust.Debt
-		if result < 0 {
-			return 0, nil
-		}
-		return result, nil
-	}
-
-	cust.Discount = false
-
-	newPirce, err := CalcPrice(*cust, 1000)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Printf("New price %d\n", newPirce)
-	}
+	fmt.Printf("%+v\n", cust)
 }
--- internal/customer.go
+++ internal/customer.go
@@ -1,20 +1,45 @@
 package internal
 
+import "errors"
+
+const DEFAULT_DISCOUNT = 500
+
 type Customer struct {
-	Name         string
-	Age          int
-	Balance      int
-	Debt         int
-	Discount     bool
-	CalcDiscount func() (int, error)
+	Name     string
+	Age      int
+	balance  int
+	debt     int
+	discount bool
+}
+
+func (c *Customer) WrOffDebt() error {
+	if c.debt >= c.balance {
+		return errors.New("not possible write off")
+	}
+
+	c.balance -= c.debt
+	c.debt = 0
+
+	return nil
+}
+
+func (c *Customer) CalcDiscount(price int) (int, error) {
+	if !c.discount {
+		return 0, errors.New("discount not available")
+	}
+	result := DEFAULT_DISCOUNT - c.debt
+	if result < 0 {
+		return 0, nil
+	}
+	return result, nil
 }
 
 func NewCustomer(name string, age int, balance int, debt int, discount bool) *Customer {
 	return &Customer{
 		Name:     name,
 		Age:      age,
-		Balance:  balance,
-		Debt:     debt,
-		Discount: discount,
+		balance:  balance,
+		debt:     debt,
+		discount: discount,
 	}
 }
